2c2e3bf899c861a48b08a94fd0ce0faa313702bf
Helen Zhao <helen4251996@yahoo.co.nz>
2017-06-10 16:18:43 +1200

    Add initial placeholder to dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'app works!';
+  activities: Array<Activity> = [{title: 'Logged in today!', content: 'You have logged in today!'}, {
+    title: 'Level up!',
+    content: 'You levelled to level 54!'
+  }];
+  friends: Array<Friend> = [{name: 'JayGradon'}, {name: 'Rashina'}, {name: 'Robert'}];
+  weeklyLeaders: Array<WeeklyLeader> = [{name: 'MarkZuckerberg', gold: 134234}, {name: 'BillGates', gold: 124345}];
+}
+
+export class Activity {
+  title: String;
+  content: String;
+}
+
+export class Friend {
+  name: String;
+}
+
+export class WeeklyLeader {
+  name: String;
+  gold: number;
 }
