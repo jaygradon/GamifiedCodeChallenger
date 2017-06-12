5c638e58bbd10beab2932b3f54b540ae5bae4b9d
Helen Zhao <helen4251996@yahoo.co.nz>
2017-06-12 12:25:24 +1200

    Refactor into landing page and dashboard, implement some landing page features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,24 +6,5 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  activities: Array<Activity> = [{title: 'Logged in today!', content: 'You have logged in today!'}, {
-    title: 'Level up!',
-    content: 'You levelled to level 54!'
-  }];
-  friends: Array<Friend> = [{name: 'JayGradon'}, {name: 'Rashina'}, {name: 'Robert'}];
-  weeklyLeaders: Array<WeeklyLeader> = [{name: 'MarkZuckerberg', gold: 134234}, {name: 'BillGates', gold: 124345}];
 }
 
-export class Activity {
-  title: String;
-  content: String;
-}
-
-export class Friend {
-  name: String;
-}
-
-export class WeeklyLeader {
-  name: String;
-  gold: number;
-}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,20 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 
 import { AppComponent } from './app.component';
+import {NavbarComponent} from './authenticated/navbar/navbar.component';
+import {DashboardComponent} from './authenticated/dashboard/dashboard.component';
+import {LandingPageNavbarComponent} from './landing-page/navbar/landing-page-navbar.component';
+import {LandingPageComponent} from './landing-page/landing-page.component';
+import {SignUpFormComponent} from './landing-page/sign-up-form/sign-up-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent,
+    LandingPageComponent,
+    LandingPageNavbarComponent,
+    SignUpFormComponent
   ],
   imports: [
     NgbModule.forRoot(),
