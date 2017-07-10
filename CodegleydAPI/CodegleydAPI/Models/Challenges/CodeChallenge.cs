a8374597ead9c213c8df01a4d2e3a98621d8b742
Jay Gradon <jaygradon@outlook.com>
2017-07-10 15:00:01 +1200

    Add Difficulty and Category fields to CodeChallenges

--- CodegleydAPI/CodegleydAPI/Models/Challenges/CodeChallenge.cs
+++ CodegleydAPI/CodegleydAPI/Models/Challenges/CodeChallenge.cs
@@ -13,6 +13,8 @@ namespace CodegleydAPI.Models
         public int ID { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public string Difficulty { get; set; }
+        public string Category { get; set; }
         public string Code { get; set; }
     }
 }
