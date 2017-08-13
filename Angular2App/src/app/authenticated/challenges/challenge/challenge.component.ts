334f24d7fa9966adae506a09084ae9f47ff58e29
Helen Zhao <helen4251996@yahoo.co.nz>
2017-08-13 21:47:29 +1200

    Add spend gold on challenge complete

--- Angular2App/src/app/authenticated/challenges/challenge/challenge.component.ts
+++ Angular2App/src/app/authenticated/challenges/challenge/challenge.component.ts
@@ -4,12 +4,14 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {Challenge} from '../../../models/Challenge';
 import {TestingService} from '../../../services/testing.service';
+import {ChallengeService} from '../../../services/challenge.service';
+import {AccountService} from '../../../services/account.service';
 
 @Component({
   selector: 'app-challenge',
   templateUrl: './challenge.component.html',
   styleUrls: ['./challenge.component.css'],
-  providers: [TestingService]
+  providers: [TestingService, AccountService]
 })
 
 export class ChallengeComponent {
@@ -18,10 +20,12 @@ export class ChallengeComponent {
   @Output() onGoBack = new EventEmitter<boolean>();
   goBack = false;
   testingService: TestingService;
+  accountService: AccountService;
   testResponse;
 
-  constructor(testingService: TestingService) {
+  constructor(testingService: TestingService, accountService: AccountService) {
     this.testingService = testingService;
+    this.accountService = accountService;
   }
 
   goBackPressed() {
@@ -31,18 +35,39 @@ export class ChallengeComponent {
 
   runCode() {
     console.log(this.challenge);
-    this.testingService.testChallenge(this.challenge).subscribe(res => this.testResponse = res);
+    this.testingService.testChallenge(this.challenge).subscribe(res => {
+      this.testResponse = res;
+      if (this.testResponse.result === 'PASS') {
+        const gold = this.getGoldReward();
+        if (gold > 0) {
+          this.accountService.incrementGold(gold).subscribe();
+        }
+      }
+    });
   }
 
   submitCode() {
     console.log('submit code');
   }
 
+  getGoldReward() {
+    let gold = 0;
+    if (this.challenge.difficulty.toLowerCase() === 'easy') {
+      gold = 10;
+    } else if (this.challenge.difficulty.toLowerCase() === 'medium') {
+      gold = 20;
+    } else if (this.challenge.difficulty.toLowerCase() === 'hard')  {
+      gold = 30;
+    }
+    return gold;
+  }
+
+
   getTestResult() {
     if (this.testResponse.result === 'FAIL') {
       return 'Test failed.';
     } else if (this.testResponse.result === 'PASS') {
-      return 'Test successfully passed';
+      return 'Test successfully passed. You have earned ' + this.getGoldReward() + ' gold!';
     } else {
       return '';
     }
